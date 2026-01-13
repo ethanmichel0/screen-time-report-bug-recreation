7bfbd016275043e44831ef8668fd81d408f3fcfa
Ethan Michel <emichel2@oberlin.edu>
2026-01-13 17:38:19 -0600

    Initial Commit

---
+++ testreproducescreentime/testreproducescreentimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  testreproducescreentimeApp.swift
+//  testreproducescreentime
+//
+//  Created by Ethan  Michel on 1/13/26.
+//
+
+import SwiftUI
+
+@main
+struct testreproducescreentimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
